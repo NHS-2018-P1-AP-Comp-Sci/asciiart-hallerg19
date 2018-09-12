efd623e573195abbaec6d09d5790ef12296c33cd
rasmussenjbsd405 <42076097+rasmussenjbsd405@users.noreply.github.com>
2018-09-12 12:07:09 -0700

    Turning in project 1.

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,54 @@
  */
 
 // YOUR CODE GOES HERE :)
+
+
+
+public class ASCIIArt {
+	public static void speech ()
+	{
+	 System.out.println("");
+	 System.out.println("");
+	 System.out.println("                            (I am not saying I am batman,         )");
+	 System.out.println("                            (I'm just saying no one has ever seen )");
+	 System.out.println("                            (me and batman in a room together.    )");
+}
+	public static void head ()
+	 {
+		 System.out.println("         /\\_______/\\      o");
+		 System.out.println("        /   _    _  \\    o");
+		 System.out.println("       /  /  \\  /  \\ \\ o");
+		 System.out.println("       |  |___\\/___| |");
+		 System.out.println("       |  \\  --   /  |");
+		 System.out.println("       \\__|___,,__|__/");
+	 }
+	public static void torso ()
+	{ 
+		System.out.println("      /----/   \\-----\\");
+		System.out.println("     /   /        \\   \\");
+		System.out.println("     |  /|  \\^^/  |\\  |");
+		System.out.println("     |OO|__________|OO|");
+		System.out.println("     |  |-----^----|  |");
+	}
+	public static void legs ()
+	{
+		System.out.println("     |   /  / \\  \\    |");
+		System.out.println("     |  /  /   \\  \\   |");
+		System.out.println("     |_OOOO_____OOOO__| ");
+	}
+	public static void quote ()
+	{
+		System.out.println("");
+		System.out.println("Always be yourself, unless you can be Batman.");
+		System.out.println("        Then always be batman.");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		speech ();
+		head ();
+		torso ();
+		legs ();
+		quote ();
+	}
+
+}
